140a4ac941c8c20d276a713277c5d7d3d5ed1d92
Tim Sundell <tim920129@hotmail.com>
2023-05-26 17:34:51 +0200

    Fixed localstorage issue

---
+++ src/components/ResetContainer.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const checkPlayers = ref(localStorage.getItem('players'));
+console.log('när appen laddas:', checkPlayers.value);
+
+const newPlayersReset = () => {
+  console.log('när funktion callas', checkPlayers.value);
+  localStorage.removeItem('players');
+  console.log('efter localstorage tömts', checkPlayers.value);
+  console.log(checkPlayers.value);
+};
+</script>
+
+<template>
+  <div class="reset-container">
+    <button>New Game (same players)</button>
+    <button @click="newPlayersReset">New Game (new players)</button>
+  </div>
+</template>
