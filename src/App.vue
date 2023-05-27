d0b3d1c960f5601f38e6d5d2581bae8ac142866f
Tim Sundell <tim920129@hotmail.com>
2023-05-27 22:10:24 +0200

    Added gameBoard component

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ import ScoreTracker from './components/ScoreTracker.vue';
 import * as data from './models/data';
 import ResetContainer from './components/ResetContainer.vue';
 import { checkPlayers } from './services/handleLocalStorage';
+import GameBoard from './components/GameBoard.vue';
 
 const onGoingGame = ref(checkPlayers);
 const players = ref(data.players);
@@ -49,14 +50,7 @@ const makeMove = (x:number,y:number) => {
       <ScoreTracker :player="player" />
     </div>
     <h2>Player {{ turn }}'s turn!</h2>
-    <div class="game-board">
-      <div v-for="(row, x) in board" :key="x">
-        <div @click="makeMove(x,y)" class="cell" v-for="(cell, y) in row" 
-        :key="y" 
-        :id="`cordinate-${x}-${y}-player-${cell}`">{{ cell }}
-        </div>
-      </div>
-    </div>
+    <GameBoard :board="board" @fillCell="makeMove"/>
     <ResetContainer @newGame="hardReset" @newRound="softReset"/>
   </div>
   <div v-else>
@@ -65,29 +59,6 @@ const makeMove = (x:number,y:number) => {
 </template>
 
 <style scoped lang="scss">
-.game-board {
-  display: flex;
-  justify-content: center;
-  font-size: 2rem;
-  .cell {
-    border: 1px solid black;
-    color: black;
-    padding: 2rem;
-    background-color: aliceblue;
-    width: 50px;
-    height: 50px;
-    &:hover {
-      background-color: beige;
-    }
-  }
-  div[id$="X"] {
-  color:#1b85b8
-}
-
-div[id$="O"] {
-  color:#e66c37
-}
-}
 .reset-container {
   margin: 1rem;
   display: flex;
---
+++ src/components/GameBoard.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+
+const {board} = defineProps(["board"])
+const emits = defineEmits(['fillCell']);
+
+const fillCell = (x:number, y:number) => {
+    emits('fillCell', x, y);
+}
+</script>
+
+<template>
+    <div class="game-board">
+      <div v-for="(row, x) in board" :key="x">
+        <div @click="fillCell(x,y)" class="cell" v-for="(cell, y) in row" 
+        :key="y" 
+        :id="`cordinate-${x}-${y}-player-${cell}`">{{ cell }}
+        </div>
+      </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.game-board {
+  display: flex;
+  justify-content: center;
+  font-size: 2rem;
+  .cell {
+    border: 1px solid black;
+    color: black;
+    padding: 2rem;
+    background-color: aliceblue;
+    width: 50px;
+    height: 50px;
+    &:hover {
+      background-color: beige;
+    }
+  }
+  div[id$="X"] {
+  color:#1b85b8
+}
+
+div[id$="O"] {
+  color:#e66c37
+}
+}
+</style>
+
+
